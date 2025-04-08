1a52019793c99528490b317ab2501926de3518f4
Alexandra Santos <aleseect@gmail.com>
2025-04-08 09:51:29 -0300

    Create data json and add commands for open notepad and json data

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import pyautogui
+import time
+from datetime import datetime
 
+pyautogui.PAUSE = .5
+
+#abre o bloco de notas
 pyautogui.press("win")
-pyautogui.write("chrome")
+pyautogui.write("bloco")
 pyautogui.press("enter")
 
-#acessa a lista de perfis e clica em um específico
-pyautogui.click(x=694, y=411)
+
+name = "Alexandra!"
+horaAtual = datetime.now().hour
+
+if 6 <= horaAtual < 18:
+    saudacao = "Bom dia, "
+else:
+    saudacao = "Boa noite, "
+
+pyautogui.write(saudacao+name)
+
+
+#importa as frases
+import pandas as pd
+
+frases = pd.read_json("frases.json", encoding="utf-8")
+
+print(frases.head())
+
+fraseAleatoria = frases.sample(n=1)
+frase = fraseAleatoria['Frase'].values[0]
+
+pyautogui.write(" Frase de hoje... ")
+pyautogui.write(frase)
